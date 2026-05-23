aa97730aaa5816f2f1ab04840676e93838e7ef6d
BryanCabrera24 <147127942+dev-bryancabrera@users.noreply.github.com>
2026-05-23 11:41:55 -0500

    actualizacion apis

--- src/modules/users/services/PersonaServices.tsx
+++ src/modules/users/services/PersonaServices.tsx
@@ -4,27 +4,27 @@ import type { PersonaData } from "../types/PersonaTypes";
 export const PersonaService = {
     /* Apis Persona */
     crearPersona: async (personaData: PersonaData) => {
-        const { data } = await bolsaEmpleoIA.post('/api/persona', personaData);
+        const { data } = await bolsaEmpleoIA.post('/persona', personaData);
         return data;
     },
 
     listarPersonas: async () => {
-        const { data } = await bolsaEmpleoIA.get('/api/persona');
+        const { data } = await bolsaEmpleoIA.get('/persona');
         return data;
     },
 
     obtenerPersona: async (id: number) => {
-        const { data } = await bolsaEmpleoIA.get(`/api/persona/${id}`);
+        const { data } = await bolsaEmpleoIA.get(`/persona/${id}`);
         return data;
     },
 
     actualizarPersona: async (id: number, personaData: Partial<PersonaData>) => {
-        const { data } = await bolsaEmpleoIA.put(`/api/persona/${id}`, personaData);
+        const { data } = await bolsaEmpleoIA.put(`/persona/${id}`, personaData);
         return data;
     },
 
     eliminarPersona: async (id: number) => {
-        const { data } = await bolsaEmpleoIA.delete(`/api/persona/${id}`);
+        const { data } = await bolsaEmpleoIA.delete(`/persona/${id}`);
         return data;
     },
 
